baf0e28a5ae80dd72d5eb9ac2756c0f6e21f1680
Athena Saldanha <asaldanha@chicagobooth.edu>
2023-04-27 16:27:54 +0000

    done

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -11,4 +11,21 @@
 #  updated_at :datetime         not null
 #
 class Actor < ApplicationRecord
+  #def my_actor
+    #a_id = self.id
+    #matching_actors = Actor.where({:id => movie_id})
+    #@the_actor = matching_actors
+  #end
+
+  def characters
+    chr_id = self.id
+    matching_characters = Character.where({:actor_id => chr_id})
+  end
+
+has_many :characters
+ def movies
+    characters.map(&:movie).uniq
+  end
+
+
 end
--- app/models/director.rb
+++ app/models/director.rb
@@ -11,4 +11,17 @@
 #  updated_at :datetime         not null
 #
 class Director < ApplicationRecord
+  
+  def my_director
+    dir_id = self.director_id
+    matching_directors = Director.where({:id => dir_id})
+    the_director = matching_directors
+  end
+  
+  
+  def filmography
+    m_id =self.id
+    matching_movies = Movie.where({:director_id => m_id})
+    return matching_movies
+  end
 end
